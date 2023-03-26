6548f77798c586e0cb0fb9c73d40a3a6aa8a0fd8
mrkike <diegoperez_96@hotmail.com>
2023-03-26 19:40:18 -0300

    migracion de proyecto anterior

---
+++ Lab.NetAbm/Lab.Net.MVC/Controllers/RickController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Net.MVC.Controllers
+{
+    public class RickController : Controller
+    {
+        // GET: Rick
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab.NetAbm/Lab.Net.MVC/Models/RickView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Razor.Generator;
+
+namespace Lab.Net.MVC.Models
+{
+    public class RickView
+    {
+       public Image  Imagen { get; set; }
+       public string Nombre { get; set; }
+       public string Genero { get; set; }
+       public string Estado { get; set; }  
+    }
+}
